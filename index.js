68b3f0c8ce9ae1e84f019913a5e0bb777f4220d6
hyzeuline <iseline.voison@gmail.com>
2026-02-11 10:47:34 +0100

    add mongodb uri

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log("Connected to Shop V2 base"))
+  .catch(err => console.error(err));
+
 const userRoutes = require("./routes/user.js");
 const productRoutes = require("./routes/product.js");
 const orderRoutes = require("./routes/order.js");
@@ -13,8 +18,6 @@ app.use(express.json());
 app.use(userRoutes);
 app.use(productRoutes);
 app.use(orderRoutes);
-
-mongoose.connect("mongodb://localhost:27017/shopjsv2");
 
 app.all("*", (req, res) => {
   res.status(404).json({ message: "This route does not exist" });
