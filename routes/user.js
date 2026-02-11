e0ee69f55d5bd5967b97d9fc98171d9b3a620a61
hyzeuline <iseline.voison@gmail.com>
2026-02-11 12:02:05 +0100

    add admin true

--- routes/user.js
+++ routes/user.js
@@ -27,7 +27,7 @@ router.post("/user/signup", async (req, res) => {
       token: token,
       hash: hash,
       salt: salt,
-      admin: false,
+      admin: true,
     });
 
     await newUser.save();
